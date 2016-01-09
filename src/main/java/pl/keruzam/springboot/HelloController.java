4bdbdaf6d90f942bfc46604157abf478f8dc1c62
Keruzam <mazurek.zg@gmail.com>
2016-01-09 22:15:31 +0100

    spring boot

---
+++ src/main/java/pl/keruzam/springboot/HelloController.java
@@ -0,0 +1,14 @@
+package pl.keruzam.springboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/")
+	public String test() {
+		return "test OK";
+	}
+
+}
